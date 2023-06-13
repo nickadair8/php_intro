956ac98fa6402f31c0f2ab79061193b6dc9f7dbc
Nick Adair <adair8@purdue.edu>
2023-06-13 15:07:56 -0400

    starting partials

--- index.php
+++ index.php
@@ -1,15 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    
-    <title>Document</title>
-</head>
-<body>
-    <h1>
-        <?php
-            echo "Hello Word";
-        ?>
-    </h1>
-</body>
-</html>
+<?php 
+
+require "views/index.view.php";
